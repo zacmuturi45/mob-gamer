8aee869871059d70b77057cec28de3689adc1a65
isaacgichuhi <zacmuturi45@gmail.com>
2023-11-11 10:40:42 +0300

    ads

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,33 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 
 function Home() {
-    return <h1>Home</h1>;
+
+useEffect(() => {
+    fetch('https://rawg-video-games-database.p.rapidapi.com/games', {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '7658f3bc224340e0a6e41c3b529762b4',
+            'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
+        }
+    })
+    .then((response) => response.json())
+    .then((resp) => console.log(resp))
+}, []);
+
+
+    return (
+        <div>
+            <input
+                className="searchbar"
+                type="text"
+                name="search"
+                placeholder="Enter movie"
+            />
+            <h1>Home</h1>
+        </div>
+    )
 }
 
 export default Home;
